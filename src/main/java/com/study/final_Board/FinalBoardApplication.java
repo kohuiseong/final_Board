041414130a3eafe01b60cc3ccdd5638589cdd816
kohuiseong <gmltjd904@gmail.com>
2024-01-10 16:22:20 +0900

    게시판 초기 세팅

---
+++ src/main/java/com/study/final_Board/FinalBoardApplication.java
@@ -0,0 +1,13 @@
+package com.study.final_Board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/final_Board/FinalBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.final_Board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
