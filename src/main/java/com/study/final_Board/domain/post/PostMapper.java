e2f9a11b8cbb6ce64afd954afeea734534470508
Jinkyu <fullmetal@kakao.com>
2024-01-12 20:27:34 +0900

    feat. 게시글 조회 기능
    mapper 커밋 누락분

--- src/main/java/com/study/final_Board/domain/post/PostMapper.java
+++ src/main/java/com/study/final_Board/domain/post/PostMapper.java
@@ -1,5 +1,7 @@
 package com.study.final_Board.domain.post;
 
+import java.util.List;
+
 public interface PostMapper {
 
   void save(PostRequest params); // 게시글 저장
@@ -7,4 +9,13 @@ public interface PostMapper {
   PostResponse findById(Long id); // 게시글 상세정보 조회
 
   void update(PostRequest params); // 게시글 수정
+
+  void deleteById(Long id);
+
+  List<PostResponse> findAll();
+
+  /*
+  게시글 수 카운팅
+   */
+  int count();
 }
