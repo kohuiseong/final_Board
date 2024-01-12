677080d20f678b5820d5b747d10b14b82cb90658
Jinkyu <fullmetal@kakao.com>
2024-01-12 19:30:21 +0900

    feat 게시글 추가 작성
    Controller Service

--- src/main/java/com/study/final_Board/domain/post/PostMapper.java
+++ src/main/java/com/study/final_Board/domain/post/PostMapper.java
@@ -1,5 +1,7 @@
 package com.study.final_Board.domain.post;
 
+import java.util.List;
+
 public interface PostMapper {
 
   void save(PostRequest params); // 게시글 저장
@@ -7,4 +9,21 @@ public interface PostMapper {
   PostResponse findById(Long id); // 게시글 상세정보 조회
 
   void update(PostRequest params); // 게시글 수정
+
+
+
+  /**
+   * 게시글 리스트 조회
+   */
+  List<PostResponse> findAll();
+
+  /**
+   * 게시글 수 카운팅
+   */
+  int count();
+
+
+  /* 게시글 삭제 */
+  void deleteByid(Long id);
 }
+
